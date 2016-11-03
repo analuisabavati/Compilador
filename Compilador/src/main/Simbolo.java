f4a0e9e8c163b82481684bd3bc27dc232df090a9
user <user@CP4036>
2016-11-03 15:20:28 -0200

    Removendo enum TipoVariavel

--- Compilador/src/main/Simbolo.java
+++ Compilador/src/main/Simbolo.java
@@ -3,7 +3,7 @@ package main;
 public class Simbolo {
 	
 	private String simbolo;
-	private TipoVariavel tipo;
+	private String tipo;
 	private String nivel;
 	
 	public String getSimbolo() {
@@ -12,10 +12,10 @@ public class Simbolo {
 	public void setSimbolo(String simbolo) {
 		this.simbolo = simbolo;
 	}
-	public TipoVariavel getTipo() {
+	public String getTipo() {
 		return tipo;
 	}
-	public void setTipo(TipoVariavel tipo) {
+	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
 	public String getNivel() {
