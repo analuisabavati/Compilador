1eb406dea9ec3fd54141f64c083003fc21262fdf
user <user@CP4036>
2016-11-03 14:52:40 -0200

    Adicionando TabelaSimbolos e TipoValiavel

---
+++ Compilador/src/main/TabelaSimbolos.java
@@ -0,0 +1,27 @@
+package main;
+
+public class TabelaSimbolos {
+	
+	private String simbolo;
+	private TipoVariavel tipo;
+	private String nivel;
+	
+	public String getSimbolo() {
+		return simbolo;
+	}
+	public void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
+	}
+	public TipoVariavel getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoVariavel tipo) {
+		this.tipo = tipo;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+}
