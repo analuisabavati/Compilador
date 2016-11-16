a04f15c1a7db0ebde7ade5f899391837ffec8dcb
user <user@CP4036>
2016-11-16 18:23:00 -0200

    Adicionando tela principal do compilador

---
+++ Compilador/src/gui/TelaPrincipal.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class TelaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenuItem mntmAbrirArquivo = new JMenuItem("Abrir arquivo...");
+		menuBar.add(mntmAbrirArquivo);
+		
+		JMenuItem mntmSalvarAlteraes = new JMenuItem("Salvar altera\u00E7\u00F5es...");
+		menuBar.add(mntmSalvarAlteraes);
+		
+		JMenuItem mntmCompilar = new JMenuItem("Compilar");
+		menuBar.add(mntmCompilar);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
